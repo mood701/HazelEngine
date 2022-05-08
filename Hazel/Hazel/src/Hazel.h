49ccf51c360b8287a884b5445e87db8050a3ee00
lupin3 <102527905+mood701@users.noreply.github.com>
2022-05-09 01:18:22 +0800

    add logger

--- Hazel/Hazel/src/Hazel.h
+++ Hazel/Hazel/src/Hazel.h
@@ -1,6 +1,7 @@
 #pragma once
 // for use by Hazel applications
 #include "Hazel/Application.h"
+#include "Hazel/Log.h"
 
 // --Entry Point--------------------------------
 #include "Hazel/EntryPoint.h"
--- Hazel/Hazel/src/Hazel/EntryPoint.h
+++ Hazel/Hazel/src/Hazel/EntryPoint.h
@@ -5,7 +5,9 @@
 extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv){
-	printf("hello world");
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Warn");
+	HZ_CORE_INFO("hello{0}", 5);
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
