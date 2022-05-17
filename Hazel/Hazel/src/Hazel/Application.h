43765d8ebe31edc0fd387df90969edb631a35ad9
lupin3 <102527905+mood701@users.noreply.github.com>
2022-05-18 00:59:20 +0800

    add close window

--- Hazel/Hazel/src/Hazel/Application.h
+++ Hazel/Hazel/src/Hazel/Application.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Core.h"
 #include "Events/Event.h"
+#include "Events/ApplicationEvent.h"
 #include "Window.h"
 
 namespace Hazel{
@@ -11,8 +12,9 @@ namespace Hazel{
 		Application();
 		virtual ~Application();
 		void Run();
-
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	
